7b9693280462e653ed372260ddbb315af6e34a7a
Anastasia Selevich <anastasia.selevich@classyllama.com>
2024-04-30 16:46:41 +0300

    Add ability to upload logo from backend

---
+++ src/Migration/Migration1714231743InquiryUploadMediaThumbnails.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace CottonArt\Inquiry\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Checkout\Shipping\ShippingMethodDefinition;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Migration\MigrationStep;
+use Shopware\Core\Framework\Uuid\Uuid;
+
+class Migration1714231743InquiryUploadMediaThumbnails extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1714231743;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $this->addDefaultMediaFolder($connection);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+
+    private function addDefaultMediaFolder(Connection $connection): void
+    {
+        $defaultFolderId = Uuid::randomBytes();
+        $configurationId = Uuid::randomBytes();
+
+        $connection->executeQuery(
+            'REPLACE INTO `media_default_folder` SET
+                id = :id,
+                entity = :entity,
+                association_fields = :association_fields,
+                created_at = :created_at;',
+            [
+                'id' => $defaultFolderId,
+                'entity' => 'inquiry_logo_placements',
+                'association_fields' => '["inquiryLogoPlacements"]',
+                'created_at' => (new \DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT),
+            ]
+        );
+
+        $connection->insert('media_folder_configuration', [
+            'id' => $configurationId,
+            'thumbnail_quality' => 80,
+            'create_thumbnails' => 1,
+            'private' => 0,
+            'created_at' => (new \DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT),
+        ]);
+
+        $connection->insert('media_folder', [
+            'id' => Uuid::randomBytes(),
+            'default_folder_id' => $defaultFolderId,
+            'name' => 'Inquiry Upload Media',
+            'media_folder_configuration_id' => $configurationId,
+            'created_at' => (new \DateTime())->format(Defaults::STORAGE_DATE_TIME_FORMAT),
+        ]);
+    }
+}
